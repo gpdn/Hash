3206cabefaa6c0aad4decf5e9b22aeeb919bcc12
gpdn <giuseppedangelo0295@gmail.com>
2024-04-27 11:54:17 +0100

    Resolved token type for token print.

--- src/debug.c
+++ src/debug.c
@@ -7,6 +7,7 @@
 static int basic_instruction(const char* name, size_t offset, FILE* file);
 static int constant_instruction(const char* name, bytecode_store_t* store, size_t offset, FILE* file);
 static inline void print_value(bytecode_store_t* store, size_t offset, value_t value);
+static const char* resolve_token_type(token_type_t type);
 
 static int basic_instruction(const char* name, size_t offset, FILE* file) {
     DEBUG_LOG("%s\n", name);
@@ -132,13 +133,74 @@ size_t disassemble_instruction(bytecode_store_t* store, size_t offset, FILE* fil
     }
 }
 
+static const char* resolve_token_type(token_type_t type) {
+    switch(type) {
+        case H_TOKEN_EOF: return "H_TOKEN_EOF";
+        case H_TOKEN_COMMA: return "H_TOKEN_COMMA";
+        case H_TOKEN_DOT: return "H_TOKEN_DOT";
+        case H_TOKEN_PLUS: return "H_TOKEN_PLUS";
+        case H_TOKEN_PLUS_PLUS: return "H_TOKEN_PLUS_PLUS";
+        case H_TOKEN_MINUS: return "H_TOKEN_MINUS";
+        case H_TOKEN_MINUS_MINUS: return "H_TOKEN_MINUS_MINUS";
+        case H_TOKEN_STAR: return "H_TOKEN_STAR";
+        case H_TOKEN_SLASH: return "H_TOKEN_SLASH";
+        case H_TOKEN_EQUAL: return "H_TOKEN_EQUAL";
+        case H_TOKEN_DOUBLE_EQUAL: return "H_TOKEN_DOUBLE_EQUAL";
+        case H_TOKEN_BANG: return "H_TOKEN_BANG";
+        case H_TOKEN_LESS: return "H_TOKEN_LESS";
+        case H_TOKEN_LESS_EQUAL: return "H_TOKEN_LESS_EQUAL";
+        case H_TOKEN_GREATER: return "H_TOKEN_GREATER";
+        case H_TOKEN_GREATER_EQUAL: return "H_TOKEN_GREATER_EQUAL";
+        case H_TOKEN_IDENTIFIER: return "H_TOKEN_IDENTIFIER";
+        case H_TOKEN_TRUE: return "H_TOKEN_TRUE";
+        case H_TOKEN_FALSE: return "H_TOKEN_FALSE";
+        case H_TOKEN_AND: return "H_TOKEN_AND";
+        case H_TOKEN_OR: return "H_TOKEN_OR";
+        case H_TOKEN_IF: return "H_TOKEN_IF";
+        case H_TOKEN_ELSE: return "H_TOKEN_ELSE";
+        case H_TOKEN_FOR: return "H_TOKEN_FOR";
+        case H_TOKEN_WHILE: return "H_TOKEN_WHILE";
+        case H_TOKEN_NULL: return "H_TOKEN_NULL";
+        case H_TOKEN_RETURN: return "H_TOKEN_RETURN";
+        case H_TOKEN_PRINT: return "H_TOKEN_PRINT";
+        case H_TOKEN_VAR: return "H_TOKEN_VAR";
+        case H_TOKEN_DO: return "H_TOKEN_DO";
+        case H_TOKEN_LEFT_PAR: return "H_TOKEN_LEFT_PAR";
+        case H_TOKEN_RIGHT_PAR: return "H_TOKEN_RIGHT_PAR";
+        case H_TOKEN_LEFT_CURLY: return "H_TOKEN_LEFT_CURLY"; 
+        case H_TOKEN_RIGHT_CURLY: return "H_TOKEN_RIGHT_CURLY";
+        case H_TOKEN_SEMICOLON: return "H_TOKEN_SEMICOLON";
+        case H_TOKEN_COLON: return "H_TOKEN_COLON";
+        case H_TOKEN_POW: return "H_TOKEN_POW";
+        case H_TOKEN_ERROR: return "H_TOKEN_ERROR";
+        case H_TOKEN_WARNING: return "H_TOKEN_WARNING";
+        case H_TOKEN_MODULO: return "H_TOKEN_MODULO";
+        case H_TOKEN_STRING_LITERAL: return "H_TOKEN_STRING_LITERAL";
+        case H_TOKEN_NUMBER_LITERAL: return "H_TOKEN_NUMBER_LITERAL";
+        case H_TOKEN_CLASS: return "H_TOKEN_CLASS";
+        case H_TOKEN_SUPER: return "H_TOKEN_SUPER";
+        case H_TOKEN_FUNCTION: return "H_TOKEN_FUNCTION";
+        case H_TOKEN_BITWISE_SHIFT_LEFT: return "H_TOKEN_BITWISE_SHIFT_LEFT";
+        case H_TOKEN_BITWISE_SHIFT_RIGHT: return "H_TOKEN_BITWISE_SHIFT_RIGHT";
+        case H_TOKEN_BITWISE_AND: return "H_TOKEN_BITWISE_AND";
+        case H_TOKEN_BITWISE_OR: return "H_TOKEN_BITWISE_OR";
+        case H_TOKEN_BITWISE_XOR: return "H_TOKEN_BITWISE_XOR";
+        case H_TOKEN_BITWISE_NOT: return "H_TOKEN_BITWISE_NOT";
+        case H_TOKEN_BANG_EQUAL: return "H_TOKEN_BANG_EQUAL";
+        case H_TOKEN_QUESTION_MARK: return "H_TOKEN_QUESTION_MARK";
+        case H_TOKEN_DOUBLE_QUESTION_MARK: return "H_TOKEN_DOUBLE_QUESTION_MARK";
+        default:
+            return "Add type";
+    }
+}
+
 void token_print(token_t* token) {
-    printf("Line: %lld - %u - '%.*s'\n", token->line, token->type, (int)token->length, token->start);
+    printf("Line: %lld - %s - '%.*s'\n", token->line, resolve_token_type(token->type), (int)token->length, token->start);
 }
 
 size_t token_write_print_string(FILE* file, token_t* token) {
     char* string = (char*)malloc((sizeof(char) * (token->length + 50)));
-    sprintf(string, "Line: %lld - %u - '%.*s'\n", token->line, token->type, (int)token->length, token->start);
+    sprintf(string, "Line: %lld - %s - '%.*s'\n", token->line, resolve_token_type(token->type), (int)token->length, token->start);
     size_t result = fwrite(string, 1, strlen(string), file);
     free((void*)string);
     return result;
